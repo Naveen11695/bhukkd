b0942cb25dc5d054eb76f0daf6ddc61cceb76c56
satyam dhawan <satyam.dhawan71@gmail.com>
2019-01-29 15:26:45 +0530

    version 0.1

---
+++ lib/Pages/TrendingPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TrendingPage extends StatefulWidget{
+
+  _TrendingPageState createState() => new _TrendingPageState();
+
+}
+
+class _TrendingPageState extends State<TrendingPage>{
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
