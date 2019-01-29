90bbfd49c1d8b245190931c6a8031bdf0c2f682c
Naveen11695 <Naveen11695@gmail.com>
2019-01-29 15:34:52 +0530

    Merge branch 'master' of E:\GitHub Repository\bhukkd with conflicts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
-import './Pages/TrendingPage.dart';
+import "package:flare_flutter/flare_actor.dart";
 
-void main(){
-  runApp(new Bhukkd());
-}
+void main() => runApp(MyApp());
 
-class Bhukkd extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bhukkd',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: SlashPage(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-class SlashPage extends StatelessWidget{
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  Widget build(BuildContext context){
-    return new SplashScreen(
-      seconds: 15,
-      backgroundColor: Colors.indigo,
-      image: Image.asset("assets/images/logo.png"),
-      navigateAfterSeconds: new HomePage(),
-    );
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class HomePage extends StatelessWidget{
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      drawer: Drawer(),
-      body: new TrendingPage(),
-    );
+  Widget build(BuildContext context) {
+    return new FlareActor("animations/boring_star.flr",
+        alignment: Alignment.center,
+        fit: BoxFit.contain,
+        animation: "rotate_scale_color");
   }
 }
