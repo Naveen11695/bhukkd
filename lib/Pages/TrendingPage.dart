61984f07b76081d51d1b5cb3598669d407acdc9c
satyam <satyam.dhawan71@gmail.com>
2019-02-12 06:12:00 -0800

    photos and menu fetched

--- lib/Pages/TrendingPage.dart
+++ lib/Pages/TrendingPage.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 // custom
 import '../Components/HorizontalScroll.dart';
 import '../api/HttpRequest.dart';
+import 'package:http/http.dart';
 import '../api/LocationRequest.dart';
 import '../models/Restruant/Restruant.dart';
+import 'package:html/parser.dart';
+
+
 class TrendingPage extends StatefulWidget {
   _TrendingPageState createState() => new _TrendingPageState();
 }
@@ -15,10 +19,40 @@ class TrendingPage extends StatefulWidget {
 void _fetchRestaurant(){
   requestRestaurant("https://developers.zomato.com/api/v2.1/restaurant?res_id=1806", "1806").then((rest){
     print(rest.restruant_Name);
-    print(rest.restruant_Id);// id is comming null
-    print(rest.restruant_Menu);
+    //print(rest.restruant_Menu);
+    fetchPhotos(rest.restruant_Photo_url);
+    fetchMenu(rest.restruant_Menu);
     //rest=restaurant;
   });
+}
+
+Future fetchPhotos(String url) async{
+  var client = new Client();
+  Response response = await client.get(url);
+  var photos = parse(response.body);
+  //thumbnails
+  List<dynamic> thumbLinks = photos.querySelectorAll('div.photos_container_load_more>div>a.res-info-thumbs');
+  //photos
+  List<dynamic> photoLinks = photos.querySelectorAll('div.photos_container_load_more>div.photobox>a.res-info-thumbs>img');
+  print("-----------extra photo info links-----------");
+  for(var link in thumbLinks){
+    print(link.attributes['href']);
+  }
+  print("----------Photo links------------");
+  for(var photoLink in photoLinks){
+    print(photoLink.attributes['data-original'].replaceAll("?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A", ""));
+  }
+}
+
+Future fetchMenu(String url) async{
+  var client = new Client();
+  Response response = await client.get(url);
+  var menu = parse(response.body);
+  List<dynamic> menuLink = menu.querySelectorAll('div#menu-image>img');
+  print("----------Menu Image-------------");
+  for(var link in menuLink){
+    print(link.attributes['src']);
+  }
 }
 
 class _TrendingPageState extends State<TrendingPage> {
