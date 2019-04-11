7069367bd799c089c759fbb5202bd85d039cf653
satyam dhawan <satyam.dhawan71@gmail.com>
2019-04-11 16:18:22 +0530

    categories alignment fixed

--- lib/Components/CategoriesComponent.dart
+++ lib/Components/CategoriesComponent.dart
@@ -4,8 +4,10 @@ class CategoriesComponent extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             new InkWell(
             onTap: (){},
@@ -27,13 +29,14 @@ class CategoriesComponent extends StatelessWidget{
         ),
         SizedBox(width: MediaQuery.of(context).size.width * 0.02,),
         new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             new InkWell(
             onTap: (){},
             child:new Container(
               color: Colors.white,
               height: MediaQuery.of(context).size.height * 0.1,
-              width: MediaQuery.of(context).size.width * 0.5,
+              width: MediaQuery.of(context).size.width * 0.4,
               child: new Image.asset("assets/images/Breakfast.png")
             ),),
             new InkWell(
@@ -41,41 +44,9 @@ class CategoriesComponent extends StatelessWidget{
               child:new Container(
                 color: Colors.white,
                 height: MediaQuery.of(context).size.height * 0.1,
-                width: MediaQuery.of(context).size.width * 0.5,
+                width: MediaQuery.of(context).size.width * 0.4,
                 child: new Image.asset("assets/images/Breakfast.png")
             )),
-            new InkWell(
-              onTap: (){},
-              child:new Container(
-                  color: Colors.white,
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: new Image.asset("assets/images/Breakfast.png")
-              ),),
-            new InkWell(
-              onTap: (){},
-              child:new Container(
-                  color: Colors.white,
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: new Image.asset("assets/images/Breakfast.png")
-              ),),
-            new InkWell(
-              onTap: (){},
-              child:new Container(
-                  color: Colors.white,
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: new Image.asset("assets/images/Breakfast.png")
-              ),),
-            new InkWell(
-              onTap: (){},
-              child:new Container(
-                  color: Colors.white,
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: new Image.asset("assets/images/Breakfast.png")
-              ),),
           ],
         ),
       ],
